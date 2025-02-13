37ddecb2ab4514efaca65ded9541edf24fb7fa5d
dxps <marius.ileana@gmail.com>
2025-02-13 13:29:39 +0200

    feat(counter-app): counter complete

--- counter-app/astro.config.mjs
+++ counter-app/astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Tell Astro that we have some dynamic state on the server (it's not all static).
+    output: "server"
+});
---
+++ counter-app/src/env.d.ts
@@ -0,0 +1,3 @@
+// Adding `counter` as a property to the global object.
+// See "Extending window and globalThis" section in https://docs.astro.build/en/guides/typescript/ page.
+declare var counter: number
