eff39100268124547bd0e2f87cec2402cc4c7cbd
dxps <marius.ileana@gmail.com>
2025-02-13 13:50:10 +0200

    feat(counter-app): client side counter mgmt
    Using Alpine.js for this.

--- counter-app/astro.config.mjs
+++ counter-app/astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
-    // Tell Astro that we have some dynamic state on the server (it's not all static).
-    output: "server"
+  // Tell Astro that we have some dynamic state on the server (it's not all static).
+  output: "server",
+
+  integrations: [alpinejs()]
 });
